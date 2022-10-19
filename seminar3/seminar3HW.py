a1c0612039785fa08c77b22531d5774a1bf95520
Ernst Avraev <avraev.ernst@yandex.ru>
2022-10-19 15:43:09 +0800

    HW finished

--- seminar3/seminar3HW.py
+++ seminar3/seminar3HW.py
@@ -23,19 +23,19 @@
 # - [2, 3, 5, 6] => [12, 15]
 
 
-arr = list(map(int, input("Введите числа через пробел:\n").split()))
+# arr = list(map(int, input("Введите числа через пробел:\n").split()))
 
-if len(arr) % 2 != 0:
-     a = len(arr)//2 + 1
-else:
-     a = len(arr)//2
+# if len(arr) % 2 != 0:
+#      a = len(arr)//2 + 1
+# else:
+#      a = len(arr)//2
 
-newArr = []
-for i in range(a):
-     newI = arr[i]*arr[len(arr)-i-1]
-     newArr.append(newI)
+# newArr = []
+# for i in range(a):
+#      newI = arr[i]*arr[len(arr)-i-1]
+#      newArr.append(newI)
 
-print(newArr)
+# print(newArr)
 
 
 # Задайте список из вещественных чисел. Напишите программу,
@@ -44,6 +44,26 @@ print(newArr)
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
+# lst = list(map(float, input("Введите числа через пробел:\n").split()))
+# newLst = []
+
+# for i in lst:
+#      if i%1 != 0:
+#           newLst.append(round(i%1, 4))
+
+# max = newLst[0]
+# min = newLst[0]
+
+# for i in newLst:
+#      if i > max:
+#           max = i
+#      elif i < min:
+#           min = i
+# print(round(max - min, 4))
+
+# new_lst = [round(i%1, 4) for i in lst if i%1 != 0]
+# print(max(new_lst) - min(new_lst))
+
 # Напишите программу, которая будет преобразовывать десятичное
 # число в двоичное.
 # Пример:
@@ -51,7 +71,36 @@ print(newArr)
 # - 3 -> 11
 # - 2 -> 10
 
+# s = " "
+# n = int(input("Input a number to convert to binary: \n"))
+# while n != 0:
+#     s = str(n % 2)+s
+#     n //= 2
+# print(s)
+
 # Задайте число. Составьте список чисел Фибоначчи, в том числе
 # для отрицательных индексов.
 # Пример:
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+def fib(n):
+     if n in [1,2]:
+          return 1
+     else:
+          return fib(n-1)+fib(n-2)
+
+def negaFib(n):
+     return ((-1)**(n+1))*fib(n)
+
+n = int(input("Введите порядковый номер числа = "))
+fibArr1 = []
+fibArr2 = []
+for e in range(1,n+1):
+     fibArr1.append(negaFib(e))
+for a in range(1,n+1):
+     fibArr2.append(fib(a))
+
+fibArr1.reverse()
+fullArr= fibArr1+fibArr2
+
+print(fullArr)
