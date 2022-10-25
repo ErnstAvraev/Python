ef80f48cd07e24bf8578f233a8f296230a3deb7e
Ernst Avraev <avraev.ernst@yandex.ru>
2022-10-25 15:38:00 +0800

    added new solutions

--- seminar2/Seminar2HW.py
+++ seminar2/Seminar2HW.py
@@ -13,6 +13,27 @@
 # print(sum)
 
 
+# Решение Гаи
+
+# print(sum([int(i) for i in input("Input number: ") if i.isdigit()]))
+
+# numb = float(input())
+# summ = 0
+# for el in str(numb):
+#     if el != '.':
+#         summ += int(el)
+# print(summ)
+
+# from curses.ascii import isdigit
+
+
+# s= input()
+# summ = 0
+# for i in s:
+#     if i.isdigit():
+#         summ+=int(i)
+# print(summ)
+
 # # 2. Напишите программу, которая принимает на вход число N
 # #  и выдает набор произведений чисел от 1 до N.
 # # Пример:
@@ -43,13 +64,69 @@
 # Позиции хранятся в файле file.txt в одной строке одно число.
 # Реализуйте алгоритм перемешивания списка.
 
-import random
+# import random
 
-N = int(input("Введите число N: "))
-arr = []
-for i in range(-N, N+1):
-    arr.append(i)
-print(arr)
+# N = int(input("Введите число N: "))
+# arr = []
+# for i in range(-N, N+1):
+#     arr.append(i)
+# print(arr)
 
-random.shuffle(arr)
-print(arr)
+# random.shuffle(arr)
+# print(arr)
+
+# another solutions:
+# 1
+
+# user_range_num = int(input("smth: "))
+# user_pos_1 = int(input("Input position 1 number: "))
+# user_pos_2 = int(input("Input position 2 number: "))
+
+# path = "file.txt"
+# data = open("file.txt", "w")
+# list = []
+# product = 1
+
+# for i in range(-user_range_num, user_range_num+1):
+#     list.append(i)
+# print(list)
+
+# for i in range(1, user_range_num*2+2):
+#     i = str(i)
+#     data.write("\n")
+#     data.write(i)
+# data.close()
+
+# 2
+
+# numN = int(input("Input number N: "))
+# list = []
+# for i in range(-numN, numN+1):
+#     list.append(i)
+# print(list)
+# position1 = int(input("input position 1: "))
+# position2 = int(input("input position 2: "))
+# print(f"multiplication of numbers = {list[(position1-1)]*list[(position2-1)]}")
+
+# 3
+
+
+
+
+# import random
+
+
+
+# n = int(input('input number: '))
+# list = []
+# for i in range(n):
+#     a = random.randint(-n,n)
+#     list.append(a)
+# print(list)
+# index_list = input(f'input position from 1 to {n} using space: ').split()
+# result = 1
+# for j in range(len(index_list)):
+#     a = int(index_list[j])-1
+#     print (f'{result}*{int(list[a])}', end = ' ')
+#     result *= int(list[a])
+# print(f'= {result}')
